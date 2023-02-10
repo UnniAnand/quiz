9e1db047131f355c9d85c3df0d86412f9bf3e6ad
UnniAnand <124883192+UnniAnand@users.noreply.github.com>
2023-02-10 11:42:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+function button1(){
+     document.getElementById("btn1").innerHTML="What's the craziest thing you have ever done?";
+     document.getElementById("btn1").style.fontSize="23px";
+     
+}
+function button2(){
+    document.getElementById("btn2").innerHTML="Which celebrity would you pick to exchange lives with and why?";
+    document.getElementById("btn2").style.fontSize="23px";
+}
+function button3(){
+    document.getElementById("btn3").innerHTML="If you could effortlessly pick up a skill/talent in an instant, What would it be?";
+    document.getElementById("btn3").style.fontSize="23px";
+}
+function button4(){
+    document.getElementById("btn4").innerHTML="If you could choose your age forever, What age would you choose and why?";
+    document.getElementById("btn4").style.fontSize="23px";
+}
+function button5(){
+    document.getElementById("btn5").innerHTML="If you could be any movie, What would it be and what character would you play?";
+    document.getElementById("btn5").style.fontSize="23px";
+}
+function button6(){
+    document.getElementById("btn6").innerHTML="What is your favorite time of the day and why?";
+    document.getElementById("btn6").style.fontSize="23px";
+}
+function button7(){
+    document.getElementById("btn7").innerHTML="If you were a superhero what would your superpower be and why?";
+    document.getElementById("btn7").style.fontSize="23px";
+}
+function button8(){
+    document.getElementById("btn8").innerHTML="If you were forced to delete all the apps on your phone and could only keep three, which one's would it be and why?";
+    document.getElementById("btn8").style.fontSize="23px";
+}
+function button9(){
+    document.getElementById("btn9").innerHTML="If you could go anywhere in the world, Where would you go and why?";
+    document.getElementById("btn9").style.fontSize="23px";
+}
+function button10(){
+    document.getElementById("btn10").innerHTML="What would you do if you win a billion dollar?";
+    document.getElementById("btn10").style.fontSize="23px";
+}
+function change1(){
+    document.getElementById("btn1").innerHTML="1";
+    document.getElementById("btn1").style.fontSize="15px";
+}
+function change2(){
+    document.getElementById("btn2").innerHTML="2";
+    document.getElementById("btn2").style.fontSize="15px";
+}
+function change3(){
+    document.getElementById("btn3").innerHTML="3";
+    document.getElementById("btn3").style.fontSize="15px";
+}
+function change4(){
+    document.getElementById("btn4").innerHTML="4";
+    document.getElementById("btn4").style.fontSize="15px";
+}
+function change5(){
+    document.getElementById("btn5").innerHTML="5";
+    document.getElementById("btn5").style.fontSize="15px";
+}
+function change6(){
+    document.getElementById("btn6").innerHTML="6";
+    document.getElementById("btn6").style.fontSize="15px";
+}
+function change7(){
+    document.getElementById("btn7").innerHTML="7";
+    document.getElementById("btn7").style.fontSize="15px";
+}
+function change8(){
+    document.getElementById("btn8").innerHTML="8";
+    document.getElementById("btn8").style.fontSize="15px";
+}
+function change9(){
+    document.getElementById("btn9").innerHTML="9";
+    document.getElementById("btn9").style.fontSize="15px";
+}
+function change10(){
+    document.getElementById("btn10").innerHTML="10";
+    document.getElementById("btn10").style.fontSize="15px";
+}
